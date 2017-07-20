16d9fdcbdd49b3c586b59c24e906db00da303132
Cong Cong Bo <cong@hippocampus.ai>
2017-07-20 15:19:44 +0100

    Player class added, attack method working

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -3,5 +3,12 @@ feature 'Attacking' do
     sign_in_and_play
     click_button 'Attack'
     expect(page).to have_content 'Dave attacked Mittens'
-    end
+  end
+
+  scenario 'reduce Player 2 HP by 10' do
+    sign_in_and_play
+    click_button 'Attack'
+    expect(page).not_to have_content 'Mittens: 60HP'
+    expect(page).to have_content 'Mittens: 50HP'
+  end
 end
